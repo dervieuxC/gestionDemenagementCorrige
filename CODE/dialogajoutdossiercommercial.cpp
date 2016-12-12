9611a330755aab816c93a83a627589a5fd470821
dervieuxC <dervieux.c@gmail.com>
2016-12-12 11:05:29 +0100

    jout fichier manquant v8

---
+++ CODE/dialogajoutdossiercommercial.cpp
@@ -0,0 +1,39 @@
+#include "dialogajoutdossiercommercial.h"
+#include "ui_dialogajoutdossiercommercial.h"
+
+DialogAjoutDossierCommercial::DialogAjoutDossierCommercial(QWidget *parent) :
+    QDialog(parent),
+    ui(new Ui::DialogAjoutDossierCommercial)
+{
+    ui->setupUi(this);
+}
+
+DialogAjoutDossierCommercial::~DialogAjoutDossierCommercial()
+{
+    delete ui;
+}
+
+QString DialogAjoutDossierCommercial::getDateDebut()
+{
+    return ui->lineEditDateDebutCommercial->text();
+}
+
+QString DialogAjoutDossierCommercial::getDateFin()
+{
+    return ui->lineEditDateFinCommercial->text();
+}
+
+QString DialogAjoutDossierCommercial::getAdrChargement()
+{
+    return ui->lineEditChargementCommercial->text();
+}
+
+QString DialogAjoutDossierCommercial::getAdrLivraison()
+{
+    return ui->lineEditLivraisonCommercial->text();
+}
+
+QString DialogAjoutDossierCommercial::getChefEquipe()
+{
+    return ui->comboBoxChefEquipe->currentText();
+}
